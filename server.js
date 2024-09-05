d441659a7faf00aaf82e05bc89c563c3da4ddd1d
Isabelcerons <125101862+Isabelcerons@users.noreply.github.com>
2024-09-04 20:49:34 -0500

    primer avance

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-const http = require('http'); // Importar el módulo http
+const http = require('http'); 
 
-// Crear un servidor
+const hostname = '127.0.0.1';
+const fs=require("fs")
+const port = 3000;
+  
+  //
+const  pagina_invalida=fs.readFileSync("pagina_invalida.html")
+const  paginaDiseño =fs.readFileSync("diseño.html")
+const  paginaDeprogramacion =fs.readFileSync("programacion.html")
+const  PaginaPrincipal =fs.readFileSync("principal.html")
+
+
 const server = http.createServer((req, res) => {
-  // Establecer el código de estado y el tipo de contenido de la respuesta
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola, Mundo!\n'); // Enviar la respuesta
+  if (req.url ==="/programacion"){
+    return res.end(paginaDeprogramacion);
+  } else if(req.url ==="/diseño"){
+    return res.end(paginaDiseño);
+  } else if (req.url ==="/"){
+    return res.end(PaginaPrincipal);
+  }else{
+  res.writeHead(404)
+  res.end(pagina_invalida)
+
+  }
 });
 
-// Asignar un puerto y hacer que el servidor escuche en ese puerto
-const port = 3000;
 server.listen(port, () => {
   console.log(`El servidor está escuchando en http://localhost:${port}`);
 });
